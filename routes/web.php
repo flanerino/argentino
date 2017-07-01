89832bef930d413b66b6659fa5b755087bd56018
fuato1 <fuato1@hotmail.com>
2017-07-01 12:48:51 -0300

    Parte de gastos lista.

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,15 @@ Route::get('/', function () {
 
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
-  
+
 Route::get('/socios', 'SociosController@show_socios');
 
 // Rutas de la parte GASTOS
 Route::prefix('gastos')->group(function(){
-  Route::get('/create-gasto', 'HomeController@createGasto')->name('create.gasto');
-  Route::post('/create-gasto', 'HomeController@storeGasto')->name('store.gasto');
-  Route::get('/lista', 'HomeController@showListaGastos')->name('gastos.lista');
-  Route::get('/edit-gasto/{gasto}', 'HomeController@editGasto')->name('edit.gasto');
-  Route::put('/edit-gasto/{gasto}', 'HomeController@updateGasto')->name('update.gasto');
-  Route::delete('/lista/{gasto}', 'HomeController@deleteGasto')->name('delete.gasto');
+  Route::get('/create-gasto', 'GastosController@createGasto')->name('create.gasto');
+  Route::post('/create-gasto', 'GastosController@storeGasto')->name('store.gasto');
+  Route::get('/lista', 'GastosController@showListaGastos')->name('gastos.lista');
+  Route::get('/edit-gasto/{gasto}', 'GastosController@editGasto')->name('edit.gasto');
+  Route::put('/edit-gasto/{gasto}', 'GastosController@updateGasto')->name('update.gasto');
+  Route::delete('/lista/{gasto}', 'GastosController@deleteGasto')->name('delete.gasto');
 });
